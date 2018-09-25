983e0a836b554d86e116d71cfcade1e962462d74
Sorbian <michal.sorbian@atos.net>
2018-09-25 12:58:14 +0200

    Began section 3

---
+++ section_3/blog/layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <TheHeader @sidenavToggle="displaySideNav = !displaySideNav"></TheHeader>
+    <TheSideNav :show="displaySideNav" @close="displaySideNav = false"></TheSideNav>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/Navigation/TheHeader';
+import TheSideNav from '@/components/Navigation/TheSideNav';
+export default {
+  components: {
+    TheHeader,
+    TheSideNav
+  },
+  data(){
+    return {
+      displaySideNav: false
+    }
+  }
+}
+</script>
+
+
+<style>
+html {
+  font-family: 'Open Sans', sans-serif;
+}
+body{
+  margin: 0;
+}
+</style>
---
+++ section_3/blog/pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Latest news</h1>
+    </section>
+    <section class="featured-posts">
+      <PostPreview id="1" thumbnail="https://contact-centres.com/wp-content/uploads/2017/06/tech.image_.june_.2017.jpg" title="Hellooo 1!" previewText="A first post!!"></PostPreview>
+      <PostPreview id="2" thumbnail="https://contact-centres.com/wp-content/uploads/2017/06/tech.image_.june_.2017.jpg" title="Hellooo 2!" previewText="A second post!!"></PostPreview>
+      <PostPreview id="3" thumbnail="https://contact-centres.com/wp-content/uploads/2017/06/tech.image_.june_.2017.jpg" title="Hellooo 3!" previewText="A third post!!"></PostPreview>
+    </section>
+  </div>
+</template>
+
+<script>
+import PostPreview from "~/components/Posts/PostPreview";
+export default {
+  components: {
+    PostPreview
+  }
+}
+
+</script>
+
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+  background-image: url('~assets/images/main-page-background.jpg');
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+
+</style>
