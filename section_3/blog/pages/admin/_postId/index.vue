d429d68b70fa8235b54affcdbb33373966d64ed1
Sorbian <michal.sorbian@atos.net>
2018-10-18 16:33:48 +0200

    Completed section 5

--- section_3/blog/pages/admin/_postId/index.vue
+++ section_3/blog/pages/admin/_postId/index.vue
@@ -1,27 +1,48 @@
 <template>
   <div class="admin-post-page">
     <section class="update-form">
-      <AdminPostForm :post="loadedPost" />
+      <AdminPostForm :post="loadedPost" @submit="onSubmitted" />
     </section>
   </div>
 </template>
 
 <script>
-import AdminPostForm from '@/components/Admin/AdminPostForm'
+import axios from 'axios';
+import AdminPostForm from '@/components/Admin/AdminPostForm';
 
 export default {
   layout: 'admin',
   components: {
     AdminPostForm
   },
-  data() {
-    return {
-      loadedPost: {
-        author: 'Maximilian',
-        title: 'My awesome Post',
-        content: 'Super amazing, thanks for that!',
-        thumbnailLink: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
-      }
+  // data() {
+  //   return {
+  //     loadedPost: {
+  //       author: 'Maximilian',
+  //       title: 'My awesome Post',
+  //       content: 'Super amazing, thanks for that!',
+  //       thumbnailLink: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
+  //     }
+  //   }
+  // }
+  asyncData(context) {
+    return axios.get('https://nuxt-blog-2d095.firebaseio.com/posts/' + context.params.postId + '.json')
+      .then(res => {
+        return {
+          loadedPost: {
+            ...res.data,
+            id: context.params.postId
+          }
+        }
+      })
+      .catch(e => context.error(e))
+  },
+  methods: {
+    onSubmitted(editedPost){
+      this.$store.dispatch('editPost', editedPost)
+        .then(() => {
+          this.$router.push('/admin');
+        });
     }
   }
 }
